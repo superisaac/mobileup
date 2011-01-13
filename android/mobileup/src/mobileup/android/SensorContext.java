21c18e3cd1e2595d0603630961cc58373fcecd88
Zeng Ke <superisaac.ke@gmail.com>
2011-01-13 21:47:52 +0800

    Added license at each of source files

--- android/mobileup/src/mobileup/android/SensorContext.java
+++ android/mobileup/src/mobileup/android/SensorContext.java
@@ -1,4 +1,11 @@
-/** %LICENSE% **/
+/** 
+ * MobileUp: A javascript development framework in android/html5 browsers
+ * Copyright (C) 2009-2010 Zeng Ke
+ * 
+ * Licensed under to term of GNU General Public License Version 2 or Later (the "GPL")
+ *   http://www.gnu.org/licenses/gpl.html
+ *
+ **/
 package mobileup.android;
 import java.io.IOException;
 
